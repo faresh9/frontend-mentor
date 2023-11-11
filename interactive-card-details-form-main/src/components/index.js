41073329a3c0b6c1db216a57681aadac7ef677fd
faresh9 <far0213025@ju.edu.jo>
2023-11-11 22:05:24 +0300

    Add Form and ThankYou components to index.js

--- interactive-card-details-form-main/src/components/index.js
+++ interactive-card-details-form-main/src/components/index.js
@@ -1,3 +1,5 @@
     export { default as Background } from './Background';
     export { default as Card } from './Card';
+    export { default as Form} from './Form';
+    export { default as ThankYou } from './ThankYou';
     
