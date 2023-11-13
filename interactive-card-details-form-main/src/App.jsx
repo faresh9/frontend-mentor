69c8564629ecfaed837a4ee106a9f3bd586c111b
faresh9 <far0213025@ju.edu.jo>
2023-11-13 21:20:42 +0300

    Delete unused files and update file paths

--- interactive-card-details-form-main/src/App.jsx
+++ interactive-card-details-form-main/src/App.jsx
@@ -1,13 +1,42 @@
-// App.jsx
-import React from 'react';
-import AppRouter from './AppRouter';
+import { useState } from 'react'
+import CardThanks from './components/CardThanks'
+import CardForm from './components/CardForm'
+import CardCredit from './components/CardCredit'
+import './styles/App.css'
 
-const App = () => {
-  return (
-    <div>
-      <AppRouter />
-    </div>
-  );
-};
+function App () {
+	const [formData, setFormData] = useState({ name: null, number: null, mm: null, yy: null, cvc: null })
+	const [validate, setValidate] = useState(false)
 
-export default App;
+	const animateSlider = (validate) => {
+		const axis = window.matchMedia('(max-width: 750px)').matches ? 'Y' : 'X'
+		document.querySelector('.cardOverflow > div').style.transform = `translate${axis}(50${axis === 'Y' ? 'vh' : 'vw'})`
+
+		document.body.classList.add('body-slider')
+
+		setTimeout(() => {
+			setValidate(validate)
+			document.body.classList.remove('body-slider')
+			document.querySelector('.cardOverflow > div').style.transform = 'translate(0)'
+		}, 500)
+	}
+
+	return (
+		<>
+			<CardCredit formData={formData} />
+			<main className='cardOverflow'>
+				<div>
+					{validate
+						? <CardThanks setFormData={setFormData} animateSlider={animateSlider} />
+						: <CardForm setFormData={setFormData} animateSlider={animateSlider} formData={formData} />
+					}
+				</div>
+			</main>
+			<footer className='attribution'>
+				<p>Made with ♥️ by <a href='https://github.com/cosmoart' target='_blank' rel='noopener noreferrer'>Cosmo</a> - <a href='https://github.com/cosmoart/Interactive-card-details-form' target='_blank' rel='noopener noreferrer'>Repository</a>
+				</p>
+			</footer>
+		</>
+	)
+}
+export default App
--- interactive-card-details-form-main/src/components/index.js
+++
@@ -1,5 +0,0 @@
-    export { default as Background } from './Background';
-    export { default as Card } from './Card';
-    export { default as Form} from './CreditCardForm';
-    export { default as ThankYou } from './ThankYou';
-    
